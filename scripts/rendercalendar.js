b42452ea220bca5fb1a868e4e542dc9147c19587
Kevin Godo <kgodo789@gmail.com>
2025-01-07 23:58:01 +0100

    Frecce funzionanti

--- scripts/rendercalendar.js
+++ scripts/rendercalendar.js
@@ -2,12 +2,22 @@ $(document).ready(function() {
     var calendarEl = document.getElementById('div-fullcalendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'timeGridDay',
+        fixedWeekCount: false,
+        dayHeaders: false,
         height: '100%',
-        headerToolbar: false,
         locale: 'it',
-        firstDay: 1
+        firstDay: 1,
+        showNonCurrentDates: false,
+        headerToolbar: false,
+        titleFormat: {
+            month: 'long',
+            year: 'numeric',
+            day: 'numeric',
+            weekday: 'long'
+        }
     });
 
+    $("#selectedDay").html(calendar.view.title);
     calendar.render();
     calendar.updateSize();
 
@@ -17,13 +27,31 @@ $(document).ready(function() {
 
     $("#radio-1").click(function() {
         calendar.changeView('timeGridDay');
+        calendar.setOption('dayHeaders', false);
+        $("#selectedDay").html(calendar.view.title);
     });
 
     $("#radio-2").click(function() {
         calendar.changeView('dayGridWeek');
+        calendar.setOption('dayHeaders', true);
+        $("#selectedDay").html(calendar.view.title);
     });
 
     $("#radio-3").click(function() {
         calendar.changeView('dayGridMonth');
+        calendar.setOption('dayHeaders', true);
+        $("#selectedDay").html(calendar.view.title);
+    });
+
+    //arretrare di 1 giorno
+    $(".day-selector-leftarrow").click(function() {
+        calendar.prev();
+        $("#selectedDay").html(calendar.view.title);
+    });
+
+    //avanzare di 1 giorno
+    $(".day-selector-rightarrow").click(function() {
+        calendar.next();
+        $("#selectedDay").html(calendar.view.title);
     });
 });
--- scripts/today.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    const giorni = ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'];
-    const mese = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
-    let today = new Date();
-    let date = giorni[today.getDay()] + ' ' + today.getDate() + ' ' + mese[today.getMonth()];
-    document.getElementById("selectedDay").innerHTML = date;
-})
